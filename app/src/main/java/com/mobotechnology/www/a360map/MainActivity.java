f98a94568bed74f12de2f39e531d8fe471d3f222
dev_bpn <jazzbpn@gmail.com>
2016-08-19 14:41:06 +0545

    ANIMATE with ZOOMING to the marker

--- app/src/main/java/com/mobotechnology/www/a360map/MainActivity.java
+++ app/src/main/java/com/mobotechnology/www/a360map/MainActivity.java
@@ -8,6 +8,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -40,14 +41,31 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         latLngArrayList.add(new LatLng(27.705630, 85.333299));
         latLngArrayList.add(new LatLng(27.728903, 85.331732));
 
+        CameraPosition cameraPosition = null;
         for (int i = 0; i < latLngArrayList.size(); i++) {
 
-            map.moveCamera(CameraUpdateFactory.newLatLngZoom(latLngArrayList.get(i), 13));
+            /** Direct zoom to label 13*/
+//            map.moveCamera(CameraUpdateFactory.newLatLngZoom(latLngArrayList.get(i), 13));
 
+
+            /**
+             * Camera position initializing
+             * */
+            cameraPosition = CameraPosition.builder()
+                    .target(latLngArrayList.get(i)).zoom(13)
+                    .bearing(90) /** Rotate map*/
+                    .build();
+
+            /** Adding multiple marker*/
             map.addMarker(new MarkerOptions()
                     .position(latLngArrayList.get(i))
                     .title("Marker"));
         }
+
+
+        /** Animate the change in camera view over 3 seconds*/
+        map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition),
+                3000, null);
 
     }
 
