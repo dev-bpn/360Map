d59777b3d34e9ec729c25b8fb817aad4fd0dab4b
dev_bpn <jazzbpn@gmail.com>
2016-08-19 11:23:59 +0545

    Marker Title

--- app/src/main/java/com/mobotechnology/www/a360map/MainActivity.java
+++ app/src/main/java/com/mobotechnology/www/a360map/MainActivity.java
@@ -9,6 +9,7 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
@@ -32,12 +33,10 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         mMap = googleMap;
         // Add a marker in Sydney, Australia, and move the camera.
-//        LatLng sydney = new LatLng(-34, 151);
-//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));  /**Display title when marker is clicked*/
-//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));  /**Display title when marker is clicked*/
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
 
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
-                new LatLng(-18.142, 178.431), 2));
 
     }
 
